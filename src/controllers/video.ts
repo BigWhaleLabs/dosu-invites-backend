152260c0398a475e740f4bd855d22d07028a3e23
T-Damer <dpancov64@gmail.com>
2021-11-25 23:50:53 +0300

    updated the code to work with video-scrolling

--- src/controllers/video.ts
+++ src/controllers/video.ts
@@ -12,13 +12,32 @@ export default class LoginController {
     const videoPath = 'src/assets/timelapse.mp4'
     try {
       if (fs.existsSync(videoPath)) {
-        ctx.body = fs.createReadStream(videoPath)
+        const range = ctx.req.headers.range
+        if (!range) {
+          ctx.throw('Provide range headers!')
+        }
+        const videoSize = fs.statSync(videoPath).size
+        const chunkSize = 10 ** 6
+
+        const start = Number(range.replace(/\D/g, ''))
+        const end = Math.min(start + chunkSize, videoSize - 1)
+
+        const contentLength = end - start + 1
+        const headers = {
+          'Content-Range': `bytes ${start}-${end}/${videoSize}`,
+          'Accept-Ranges': 'bytes',
+          'Content-Length': contentLength,
+          'Content-Type': 'video/mp4',
+        }
+
+        ctx.res.writeHead(206, headers)
+        const videoStream = fs.createReadStream(videoPath, { start, end })
+        videoStream.pipe(ctx.res)
       } else {
         throw 'Video not found'
       }
-
-      return ctx.body
     } catch (error) {
+      ctx.status = 400
       console.error(error)
       return error
     }
