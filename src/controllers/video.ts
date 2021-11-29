ec9c5a639e5c50df43982fe52af7fecc9081abea
T-Damer <dpancov64@gmail.com>
2021-11-29 22:17:16 +0300

    return cutted video

--- src/controllers/video.ts
+++ src/controllers/video.ts
@@ -3,40 +3,60 @@ import * as fs from 'fs'
 import { Context } from 'koa'
 import { Controller, Ctx, Get } from 'amala'
 import { ethers } from 'ethers'
+import ffmpegPath = require('@ffmpeg-installer/ffmpeg')
+import ffmpeg = require('fluent-ffmpeg')
 
+ffmpeg.setFfmpegPath(ffmpegPath.path)
 const nftInvites = 320
+const videoPath = process.env.VIDEO_PATH
+const cutVideoPath = 'src/assets/videoCut/timelapse.mp4'
 
 @Controller('/video')
 export default class LoginController {
   @Get('/')
   createVideoStream(@Ctx() ctx: Context) {
-    const videoPath = 'src/assets/timelapse.mp4'
     try {
-      if (fs.existsSync(videoPath)) {
-        const range = ctx.req.headers.range
-        if (!range) {
-          ctx.throw(406)
-        }
-        const videoSize = fs.statSync(videoPath).size
-        const chunkSize = 10 ** 6
-
-        const start = Number(range.replace(/\D/g, ''))
-        const end = Math.min(start + chunkSize, videoSize - 1)
-
-        const contentLength = end - start + 1
-        const headers = {
-          'Content-Range': `bytes ${start}-${end}/${videoSize}`,
-          'Accept-Ranges': 'bytes',
-          'Content-Length': contentLength,
-          'Content-Type': 'video/mp4',
-        }
-
-        ctx.res.writeHead(206, headers)
-        const videoStream = fs.createReadStream(videoPath, { start, end })
-        return videoStream
-      } else {
+      if (!fs.existsSync(videoPath)) {
         throw 'Video not found'
       }
+      const range = ctx.req.headers.range
+      if (!range) {
+        ctx.throw(406)
+      }
+
+      if (!fs.existsSync(cutVideoPath)) {
+        ffmpeg(videoPath)
+          .setStartTime(0)
+          .setDuration(nftInvites / 24)
+          .output(cutVideoPath)
+          .on('error', function (error: Error) {
+            console.error('error: ', error)
+          })
+          .on('end', function (err) {
+            if (!err) {
+              console.log('conversion Done')
+            }
+          })
+          .run()
+      }
+
+      const videoSize = fs.statSync(cutVideoPath).size
+      const chunkSize = 10 ** 6
+
+      const start = Number(range.replace(/\D/g, ''))
+      const end = Math.min(start + chunkSize, videoSize - 1)
+
+      const contentLength = end - start + 1
+      const headers = {
+        'Content-Range': `bytes ${start}-${end}/${videoSize}`,
+        'Accept-Ranges': 'bytes',
+        'Content-Length': contentLength,
+        'Content-Type': 'video/mp4',
+      }
+
+      ctx.res.writeHead(206, headers)
+      const videoStream = fs.createReadStream(cutVideoPath, { start, end })
+      return videoStream
     } catch (error) {
       ctx.status = 400
       console.error(error)
