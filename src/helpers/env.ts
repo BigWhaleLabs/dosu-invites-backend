3acf76e8b294ba4f4a0627d1dbee0ecfc3b5af86
Nikita Kolmogorov <backmeupplz@gmail.com>
2021-11-30 12:03:05 -0800

    fix dotenv

--- src/helpers/env.ts
+++ src/helpers/env.ts
@@ -1,7 +1,7 @@
 import * as dotenv from 'dotenv'
 import { cleanEnv, num } from 'envalid'
 
-dotenv.config({ path: `${__dirname}/../.env` })
+dotenv.config({ path: `${__dirname}/../../.env` })
 
 // eslint-disable-next-line node/no-process-env
 export default cleanEnv(process.env, {
