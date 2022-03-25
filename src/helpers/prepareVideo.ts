844b2c7c91c48c30804eaa258dcfd6fdd5c3a602
Daniil Pankov <dpancov64@gmail.com>
2022-03-25 23:19:20 +0300

    Video from frames (#4)
    * Implement saving cutted video into IPFS
    
    * updated for better typings
    
    * now use only core
    
    * little code update
    
    * Used infura to connect and use IPFS
    
    * updated readme
    
    * removed unnecessary await
    
    * updated import
    
    * update if new invite is generated
    
    * save frames into the folder and add them into IPFS
    
    * Save with name of the owner
    
    * actually use cache
    
    * trying to trick git to track cutVideoFrames folder
    
    * tricked the git, now folder remains
    
    * await on cache update
    
    * save with another name
    
    * display links to the frames
    
    * added types for ffmpeg-extract-frame
    
    * upgraded the packages
    
    * added types for abi and contract itself
    
    * edited tsconfig again, also fixed imports
    
    * ignore all files insdie the folder
    
    * run prettier after generating types and exclude abi.json from esling parser
    
    * try to run prettier in another sequence
    
    * dont use prettier
    
    * trying to trick the yarn
    
    * another try
    
    * push the types into the repo
    
    * trying to work without the types
    
    * completly removed the types, let's see if this will work
    
    * abi.json must be included
    
    * generate the types, then fix them
    
    * generate types before compiling, actually
    
    * better readme
    
    * generate types before developing
    
    * updated contract abi
    
    * added local ipfs node
    
    * don't use contract function before it's completed
    
    * now it actually saves to IPFS and publishes to IPNS
    
    * updated contract ABI
    
    * added info about ipfs app
    
    * probably fixed noble/secp error
    
    * trying to fix noble-secp again
    
    * recreate yarn.lock
    
    * updated according to the new frontend
    
    * added ipfs address into env
    
    * more info about ipfs
    
    * make saving of frames run asynchronous
    
    * save specified file after new mint is catched
    
    * little renaming
    
    * create client once, get multiple times
    
    * we can't save one frame, we need to save every of them
    
    * final form
    
    * removed comments and console.log
    
    * removed unnecessary pins
    
    * saved last version of the contract in the env.example
    
    * remapped to +1 with addresses
    
    * port defaults to 1336
    
    * given node engine version
    
    * updated packages
    
    * dont lint the json
    
    * additionaly lets ignore all mp4 files
    
    * going back to the 0 starting frame
    
    * check before tyring to get the tokenURI, because token may be non-existing
    
    * added check it you set baseURI
    
    * updated info for mac
    
    * updated to use allowlist
    
    * use cid version 1
    
    * let's use another gateway
    
    * lets use postinstall script
    
    * fix typo
    
    * generate types right after the installation
    
    * lets use 'fix'
    
    * generate types when developing
    
    * used console.error
    
    * lets use built in jsipfs
    
    * use node lts version under 17
    
    * removed unnecessary methods, we can remove almost everything, except ipfs
    
    * Break video into frames, then merge them into video with fps=1
    
    * create a video from the frames
    
    * declare type notations
    
    * tweaked to the end
    
    * better folder handling
    
    * upd
    
    * give proper names to the frames
    
    * save frames in the lower case
    
    * better code styles
    
    * remove 0.1 seconds from the video length
    
    * fix-types
    
    * little update
    
    * copy date upd
    
    * don't fix types, ignore styles
    
    * lets do this
    
    * use include

--- src/helpers/prepareVideo.ts
+++ src/helpers/prepareVideo.ts
@@ -1,37 +1,81 @@
 import * as ffmpeg from 'fluent-ffmpeg'
 import * as ffmpegPath from '@ffmpeg-installer/ffmpeg'
-import { cutVideoPath } from '@/helpers/localPath'
-import { cwd } from 'process'
-import { existsSync, unlinkSync } from 'fs'
-import { resolve } from 'path'
-import getVideoLength from '@/helpers/getVideoLength'
+import * as ffprobe from '@ffprobe-installer/ffprobe'
+import * as videoshow from 'videoshow'
+import { TokenToAddressMap } from '@/helpers/contract'
+import {
+  copyFileSync,
+  existsSync,
+  mkdirSync,
+  readdirSync,
+  unlinkSync,
+} from 'fs'
+import {
+  cutVideoFramesPath,
+  cutVideoPath,
+  framesPath,
+  tmpVideoPath,
+} from '@/helpers/localPath'
 
 ffmpeg.setFfmpegPath(ffmpegPath.path)
+ffmpeg.setFfprobePath(ffprobe.path)
 
-const videoPath = resolve(cwd(), 'video', 'timelapse.mp4')
-
-export default async function prepareVideo(videoLength?: number) {
+export default function prepareVideo(tokenToAddressMap: TokenToAddressMap) {
   try {
-    if (!videoLength) {
-      videoLength = await getVideoLength()
-    }
-
     return new Promise<void>((resolve, reject) => {
-      // Check if we have input
-      if (!existsSync(videoPath)) return reject(new Error('Video not found'))
+      if (!existsSync(framesPath)) throw new Error('Frames folder not found')
 
-      // Clean output if needed
+      // Clean output
+      existsSync(cutVideoFramesPath)
+        ? readdirSync(cutVideoFramesPath).forEach((file) =>
+            unlinkSync(`${cutVideoFramesPath}/${file}`)
+          )
+        : mkdirSync(cutVideoFramesPath)
       if (existsSync(cutVideoPath)) unlinkSync(cutVideoPath)
 
-      // Do the cutting
-      ffmpeg(videoPath)
-        .setStartTime(0)
-        .withVideoFilter('setpts=24.0*PTS')
-        .setDuration(videoLength)
-        .output(cutVideoPath)
-        .on('error', (error) => reject(error))
-        .on('end', (error) => (error ? reject(error) : resolve()))
-        .run()
+      // Take only minted frames
+      const allFrames = readdirSync(framesPath)
+      const tokenToAddressMapValues = Object.values(tokenToAddressMap)
+      tokenToAddressMapValues.forEach((address, index) =>
+        copyFileSync(
+          `${framesPath}/${allFrames[index]}`,
+          `${cutVideoFramesPath}/${index}-${address.toLowerCase()}.png`
+        )
+      )
+
+      const frames = readdirSync(cutVideoFramesPath).map(
+        (frame) => `${cutVideoFramesPath}/${frame}`
+      )
+
+      // Merge frames into the video with 1image/24frames
+      const videoOptions = {
+        fps: 60,
+        loop: 1,
+        transition: false,
+        videoBitrate: 1655,
+        videoCodec: 'libx264',
+        size: '400x400',
+        format: 'mp4',
+        pixelFormat: 'yuv420p',
+      }
+
+      videoshow(frames, videoOptions)
+        .save(tmpVideoPath)
+        .on('error', (error: string) => {
+          console.error(error)
+          reject()
+        })
+        .on('end', () =>
+          ffmpeg(tmpVideoPath)
+            .duration(tokenToAddressMapValues.length - 0.1) // Reduce duration to work properly on the frontend
+            .output(cutVideoPath)
+            .on('error', (error) => {
+              console.error(error)
+              reject()
+            })
+            .on('end', () => resolve())
+            .run()
+        )
     })
   } catch (error) {
     console.error(error)
